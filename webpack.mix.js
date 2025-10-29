f7f1a0d5a88bf2966cfba64ce130ce7d48570193
jofremarques <jofremartinsmarques@gmail.com>
2025-10-29 10:31:50 -0300

    feat updates

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,10 +3,12 @@ const mix = require("laravel-mix");
 mix.sass("public/sass/app.scss", "public/dist/frontend/css/")
     .options({
         processCssUrls: false,
+         clearConsole: false
     })
     .sourceMaps();
 mix.sass("public/themes/mytravel/sass/app.scss", "public/themes/mytravel/dist/frontend/css")
     .options({
-        processCssUrls: false,
+        processCssUrls: false, 
+        clearConsole: false
     })
     .sourceMaps();
